90ff473c5093e5de1f2677a305a0d0103f5f1089
Ryan Jacobs <ryan@rmj.us>
2020-12-19 13:09:14 -0800

    vnc

--- redbird/app.js
+++ redbird/app.js
@@ -23,6 +23,7 @@ const ssl = {
 }
 
 // port 6081 is the default varnish port on CentOS 8.2 systemd
+proxy.register("vnc.zfs.rent",     "http://localhost:6080",      ssl);
 proxy.register("zfs.rent",         "http://localhost:6081",      ssl);
 proxy.register("radious.dev",      "http://localhost:6081",      ssl);
 proxy.register("blog.notryan.com", "http://mir.radious.co:2020", ssl);
