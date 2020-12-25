c10613db0ba8f45242f9d8add8af028839ab4357
Ryan Jacobs <ryan@rmj.us>
2020-12-24 23:18:11 -0800

    use origin

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,10 +2,11 @@
 set -e
 
 ./build.sh
+origin="git@github.com:radious-subsystems/zfs.rent"
 
 cd dist
 git init
 git add .
 git commit -m "deploy: $(date)"
-git push --force git@github.com:radious-subsystems/zfs.rent master:frontend-live
+git push --force "$origin" master:frontend-live
 rm -rf .git
