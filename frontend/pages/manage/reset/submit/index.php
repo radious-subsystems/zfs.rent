a17402d9468bc22ed9ba21fc639028b273d3f241
Ryan Jacobs <ryan@rmj.us>
2020-12-24 23:23:02 -0800

    no php

--- frontend/pages/manage/reset/submit/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$uuid     = $_POST['uuid'];
-$password = $_POST['password'];
-
-$cmd  = '/home/ryan/zfs-rent/api/internal/php_set_password_via_uuid.rb';
-$ecmd = escapeshellcmd(implode(' ', array($cmd, $uuid, $password)));
-
-system($ecmd);
-?>
