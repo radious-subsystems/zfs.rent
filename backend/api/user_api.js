3cac52a2d436220073cf55c6738ab1000356ab45
Ryan Jacobs <ryan@rmj.us>
2020-12-25 09:17:05 -0800

    wip

--- backend/api/user_api.js
+++ backend/api/user_api.js
@@ -4,7 +4,7 @@ const pool = new Pool();
 
 // register routes
 module.exports = app => {
-  app.post("/session_details", session_details);
+  app.get("/session_details", session_details);
 }
 
 async function session_details(req, res) {
@@ -12,7 +12,9 @@ async function session_details(req, res) {
     SELECT
       s.id as web_session_id,
       (CURRENT_TIMESTAMP > s.expires_at) as is_web_session_expired,
-      (s.expires_at - current_timestamp) as web_session_expires_in,
+      (EXTRACT(EPOCH FROM
+        (s.expires_at - current_timestamp))::REAL/3600
+      )::NUMERIC(9,2) as web_session_hours_left,
       u.api_key,
       u.uid,
       u.email
@@ -21,6 +23,6 @@ async function session_details(req, res) {
       USING(uid)
     WHERE s.id = $1
   `;
-  const row = (await pool.query(query, [req.body.session_id])).rows[0];
-  res.json({row});
+  const row = (await pool.query(query, [req.query.session_id])).rows[0];
+  res.json(row);
 }
