2770255794d3ad30eb362807e418f505ee9c0656
Ryan Jacobs <ryan@rmj.us>
2020-12-25 09:12:41 -0800

    cores

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,9 +3,12 @@
 const public_api_init = require("./public_api");
 const login_api_init  = require("./login_api");
 const user_api_init   = require("./user_api");
+
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
