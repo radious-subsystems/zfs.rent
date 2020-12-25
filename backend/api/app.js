fc72beb0a4bc6e4865011604301ef62febc7c607
Ryan Jacobs <ryan@rmj.us>
2020-12-25 07:52:59 -0800

    rm localost

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const public_api_init = require("./public_api");
+const login_api_init  = require("./login_api");
 const express = require("express");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 public_api_init(app);
+login_api_init(app);
+
 app.listen(2520);
---
+++ backend/api/login_api.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const {Pool} = require("pg");
+const pool = new Pool();
+
+// register routes
+module.exports = app => {
+  app.post("/login",   login_req);
+  app.get("/session", session_open);
+}
+
+// account login
+async function login_req(req, res) {
+  console.log(req.body);
+  const email = req.body.email;
+
+  if (email == null) {
+    res.status(400).send(`error: invalid email: ${email}`);
+    return;
+  }
+
+  try {
+    await enqueue_login_req(email);
+    res.send("login request received. please check your email.");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("error: unable to queue login request.");
+  }
+};
+
+async function enqueue_login_req(email) {
+  const query = `
+    INSERT INTO work_queue (req_type, req_json)
+    VALUES ('login_req', $1);
+  `;
+  const req_json = JSON.stringify({email});
+  await pool.query(query, [req_json]);
+}
+
+async function session_open(req, res) {
+  const session_id = req.query?.id;
+  console.log({cookies: req.cookies, session_id});
+
+  res.cookie("session_id", session_id);
+  res.cookie("Domain",     "zfs.rent");
+
+  res.redirect("https://zfs.rent/manage.html");
+}
