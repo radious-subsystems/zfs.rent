8cfb1f55cae6ae84364379ba816fc8b50555328a
Ryan Jacobs <ryan@rmj.us>
2021-01-01 21:50:16 -0800

    index.js

--- backend/netlify_functions/debug/index.js
+++ backend/netlify_functions/debug/index.js
@@ -2,6 +2,6 @@ exports.handler = function (event, context) {
   return {
     statusCode: 200,
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({event, context})
+    body: JSON.stringify(event)
   };
 }
