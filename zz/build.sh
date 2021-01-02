17d50d2c988aa4def8ba0993ef8df20c6398e2c4
Ryan Jacobs <ryan@rmj.us>
2021-01-01 22:45:38 -0800

    compress

--- zz/build.sh
+++ zz/build.sh
@@ -6,9 +6,16 @@ cd "$(dirname "$BASH_SOURCE")"
 git rev-parse --short HEAD | tee lib/assets/GIT_COMMIT_REF
 date +%s                   | tee lib/assets/BUILD_DATE
 
+# install fresh deps and upsert the version
 npm ci
 npm version --allow-same-version $(cat VERSION)
+
+# generate binary
 npx pkg .\
     --target node14-linux\
     --output dist/zz.bin\
     --options unhandled-rejections=strict
+
+# compress binary in a couple of formats
+gzip   --keep -v dist/zz.bin
+xz -T0 --keep -v dist/zz.bin
