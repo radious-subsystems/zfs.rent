7436199b3ff4759cd6d57b176d78a064c4b2cd60
Ryan Jacobs <ryan@rmj.us>
2021-01-02 06:05:22 -0800

    ip link

---
+++ backend/db/views/zfs_rent/_ip_link_stats.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+  DROP VIEW IF EXISTS _ip_link_stats;
+CREATE VIEW _ip_link_stats AS (
+  WITH s1 AS (
+    SELECT
+      to_timestamp(unix_time) as ts,
+      jsonb_array_elements(jsn) as content
+    FROM metrics
+    WHERE
+      cmd = 'ip -stats -json link show'
+  )
+
+  SELECT
+    ts,
+    --content->>'ifindex'   as index,
+    --content->'flags'      as flags,
+    --content->>'link_type' as type,
+    content->>'address' as addr,
+    content->>'ifname'  as name,
+    ((content#>>'{stats64,rx,bytes}')::REAL/1e9)::NUMERIC(9,2) as rx_gb,
+    ((content#>>'{stats64,tx,bytes}')::REAL/1e9)::NUMERIC(9,2) as tx_gb
+  FROM s1
+  WHERE
+    content->>'ifname' LIKE 'vnet%'
+);
+
+COMMIT;
