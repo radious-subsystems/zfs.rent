86436a8910c277bfa36ebf1931d3fccef1bcc011
Ryan Jacobs <ryan@rmj.us>
2021-01-02 07:16:48 -0800

    user bw

--- backend/db/views/zfs_rent/_user_bandwidth.sql
+++ backend/db/views/zfs_rent/_user_bandwidth.sql
@@ -4,16 +4,30 @@ BEGIN;
 DROP VIEW IF EXISTS _user_bandwidth;
 
 CREATE VIEW _user_bandwidth AS (
+  WITH s1 as (
+    SELECT
+      ts::date as date, addr,
+      max(rx_gb + tx_gb) as gb_consumed
+    FROM _ip_link_stats
+    GROUP BY 1, 2
+    ORDER BY 1 ASC
+  )
+
   SELECT
-    ts::date, addr,
-    max(rx_gb) as rx_gb,
-    max(tx_gb) as tx_gb
-  FROM _ip_link_stats
-  JOIN 
-  GROUP BY 1, 2
-  ORDER BY 1, 2 DESC
+    date, domain,
+    gb_consumed -
+    (CASE
+      WHEN LAG(gb_consumed) OVER (PARTITION BY addr ORDER BY date) IS NULL
+          THEN 0
+      ELSE LAG(gb_consumed) OVER (PARTITION BY addr ORDER BY date)
+    END) as gb_consumed
+  FROM s1
+  LEFT JOIN dhcp_src
+    ON SUBSTRING(dhcp_src.mac, 10) = SUBSTRING(s1.addr, 10)
+  WHERE domain IS NOT NULL
+  ORDER BY domain ASC, date ASC
 );
 
-SELECT * FROM _ip_link_deriv;
+SELECT * FROM _user_bandwidth;
 
 COMMIT;
