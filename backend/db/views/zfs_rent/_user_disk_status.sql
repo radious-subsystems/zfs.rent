096017ccf180dafc424ae5b0a8d492136c8505ea
Ryan Jacobs <ryan@rmj.us>
2021-01-02 02:44:55 -0800

    view

---
+++ backend/db/views/zfs_rent/_user_disk_status.sql
@@ -0,0 +1,12 @@
+CREATE VIEW _user_disk_status AS (
+  SELECT
+    dhcp.ip,
+    disk.domain,
+    disk.model,
+    disk.serial,
+    h.ts as sampled_at,
+    h.temp_c
+  FROM disks disk
+    JOIN _hddtemp_current h USING (serial)
+    JOIN dhcp_src dhcp USING (domain)
+);
