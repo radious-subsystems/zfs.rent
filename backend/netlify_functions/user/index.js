1902f9a65add50d4c2c98aee1022cbead6e7b65d
Ryan Jacobs <ryan@rmj.us>
2021-01-01 20:54:28 -0800

    fix

--- backend/netlify_functions/user/index.js
+++ backend/netlify_functions/user/index.js
@@ -1,13 +1,15 @@
-const fs = require("fs");
+const fs   = require("fs");
+const path = require("path");
 
 exports.handler = function (event, context) {
   const parts   = event.path.split('/');
   const subPath = parts[parts.length-1];
 
   // subpath not implemented --> 404
-  if (!fs.existsSync(subPath))
+  const js = `./${subPath}.js`;
+  if (!fs.existsSync(path.join(__dirname, js)))
     return {statusCode: 404, body: '/user not implemented'};
 
   // otherwise, include and execute the subpath
-  return (require(`./${subPath}`)).handler(event, context);
+  return (require(js)).handler(event, context);
 }
