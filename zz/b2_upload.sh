1634b4676de47030d57d6186baf5050c61964b4e
Ryan Jacobs <ryan@rmj.us>
2021-01-01 22:46:06 -0800

    b2

--- zz/b2_upload.sh
+++ zz/b2_upload.sh
@@ -12,5 +12,7 @@ fi
 # login
 "$b2" authorize-account "$b2_key_id" "$b2_app_key"
 
-# upload
-"$b2" upload-file "zfs-rent" "dist/zz.bin" "zz"
+# upload files
+"$b2" upload-file "zfs-rent" "dist/zz.bin"    "zz"
+"$b2" upload-file "zfs-rent" "dist/zz.bin.gz" "zz.gz"
+"$b2" upload-file "zfs-rent" "dist/zz.bin.xz" "zz.xz"
