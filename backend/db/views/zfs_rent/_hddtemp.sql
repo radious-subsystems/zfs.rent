007712f12b8614a2a6daf366c5dd4c1beed21cfe
Ryan Jacobs <ryan@rmj.us>
2021-01-02 02:01:41 -0800

    view

--- backend/db/views/zfs_rent/_hddtemp.sql
+++ backend/db/views/zfs_rent/_hddtemp.sql
@@ -1,28 +1,35 @@
--- Create timeseries of drive_name->temp_c
-WITH s1 as (
-  SELECT
-    to_timestamp(unix_time) as ts,
-    hostname,
-    split_part(line, ':', 1) as name,
-    regexp_replace(
-      split_part(line, ':', 3),
-      '[^0-9.]+', '', 'g'
-    )::REAL as temp_c
-  FROM
-    metrics,
-    unnest(string_to_array(txt, E'\n')) as x(line)
-  WHERE
-    cmd = 'hddtemp -w' AND
-    line != ''
-)
+BEGIN;
 
--- Join serial data onto table
-SELECT
-  s1.ts,
-  s1.hostname,
-  cd.model,
-  cd.serial,
-  s1.temp_c
-FROM s1
-LEFT JOIN _connected_disk cd
-  ON s1.name = '/dev/' || cd.name
+  DROP VIEW IF EXISTS _hddtemp;
+CREATE VIEW _hddtemp AS (
+  -- Create timeseries of drive_name->temp_c
+  WITH s1 as (
+    SELECT
+      to_timestamp(unix_time) as ts,
+      hostname,
+      split_part(line, ':', 1) as name,
+      regexp_replace(
+        split_part(line, ':', 3),
+        '[^0-9.]+', '', 'g'
+      )::REAL as temp_c
+    FROM
+      metrics,
+      unnest(string_to_array(txt, E'\n')) as x(line)
+    WHERE
+      cmd = 'hddtemp -w' AND
+      line != ''
+  )
+
+  -- Join serial data onto table
+  SELECT
+    s1.ts,
+    s1.hostname,
+    cd.model,
+    cd.serial,
+    s1.temp_c
+  FROM s1
+  LEFT JOIN _connected_disk cd
+    ON s1.name = '/dev/' || cd.name
+);
+
+COMMIT;
