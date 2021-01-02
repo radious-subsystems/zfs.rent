ad4f2892d9262a00b4e31edd3eeb29a9a2dd1d51
Ryan Jacobs <ryan@rmj.us>
2021-01-02 01:08:43 -0800

    log metrics

---
+++ backend/hypervisor/metrics/metrics.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require "pg"
+require "open3"
+
+pg = PG.connect()
+rows = pg.exec(%{SELECT * FROM metrics_cmd}).to_a
+
+rows.each do |row|
+    # fork and execute command --> insert to postgres
+    pid = fork do
+        cmd = row["command"]
+        stdout, stderr, status = Open3.capture3(cmd)
+        puts status
+
+        # map stdout to the correct column
+        txt = (row["output_type"] == "text") ? stdout : nil
+        jsn = (row["output_type"] == "json") ? stdout : nil
+        xml = (row["output_type"] == "xml")  ? stdout : nil
+
+        # insert
+        pg.exec(%{
+            INSERT INTO metrics
+            (hostname, cmd, status, stderr, txt, jsn, xml)
+            VALUES ($1, $2, $3, $4, $5, $6, $7);
+        }, [`echo $HOSTNAME`, cmd, status, stderr, txt, jsn, xml])
+    end
+end
+
+Process.wait
+sleep 1
---
+++ backend/hypervisor/metrics/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/zfs.rent.private/prod.env
+ruby metrics.rb
