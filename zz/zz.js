97be4bca96e09b960adc6574bb38eddca6de96b5
Ryan Jacobs <ryan@rmj.us>
2021-01-02 08:19:34 -0800

    format

--- zz/zz.js
+++ zz/zz.js
@@ -13,14 +13,14 @@ const fetch = require("node-fetch");
 (async function() {
   let res, jsn, m;
 
-  console.log("bandwidth: daily");
+  console.log("bandwidth -> daily:");
   res = await fetch("https://zfs.rent/api/v0/user/bandwidth-daily");
   jsn = await res.json();
   m = jsn.map(x => ({[x.date]: {domain: x.domain, gb_consumed: parseFloat(x.gb_consumed)}}))
          .reduce((a,b,{}) => Object.assign(a, b));
   console.table(m);
 
-  console.log("\nbandwidth: monthly");
+  console.log("\nbandwidth -> monthly:");
   res = await fetch("https://zfs.rent/api/v0/user/bandwidth-monthly");
   jsn = await res.json();
   m = jsn.map(x => ({[x.month]: {domain: x.domain, gb_consumed: parseFloat(x.gb_consumed)}}))
