3cab260e8f5dffc36ebac780299f51b10e845d77
Ryan Jacobs <ryan@rmj.us>
2021-01-01 23:26:44 -0800

    rows

--- backend/netlify_functions/user/info.js
+++ backend/netlify_functions/user/info.js
@@ -5,16 +5,16 @@ const {pg_netlify_functions_log} = require("../lib/helper");
 exports.handler = async function (event, context) {
   pg_netlify_functions_log(event, context);
 
-  let res;
-  try {
-    res = (await pool.query('select version()')).rows[0];
-  } catch (e) {
-    res = String(e);
-  }
+  // Grab user info
+  const ip = event["headers"]["ip"];
+  const query = `SELECT * FROM dhcp_src WHERE ip = $1`;
+  const rows = await pool.query(query, [ip])
+    .then(res => res.rows[0])
+    .catch(e => String(e));
 
   return {
     statusCode: 200,
     headers: {'content-type': 'application/json'},
-    body: JSON.stringify({res, git: process.env.COMMIT_REF})
+    body: JSON.stringify({...rows, git: process.env.COMMIT_REF})
   };
 }
