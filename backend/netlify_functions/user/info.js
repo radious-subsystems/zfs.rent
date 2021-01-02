a50a1f6d152649fbab00e6fe58017f6f87aa2fc2
Ryan Jacobs <ryan@rmj.us>
2021-01-01 21:06:35 -0800

    caps

--- backend/netlify_functions/user/info.js
+++ backend/netlify_functions/user/info.js
@@ -1,5 +1,5 @@
 const {Pool} = require("pg");
-const pool = new Pool({connectionString: process.env.pg_url});
+const pool = new Pool({connectionString: process.env.PG_URL});
 
 exports.handler = async function (event, context) {
   let res;
