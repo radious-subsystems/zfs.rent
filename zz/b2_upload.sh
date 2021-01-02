43059b954dd2a347150cb23e92dbe3ddb75354f8
Ryan Jacobs <ryan@rmj.us>
2021-01-01 22:40:37 -0800

    consolidate b2 scripts

--- zz/b2_upload.sh
+++ zz/b2_upload.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-./b2 authorize-account "$b2_key_id" "$b2_app_key"
-./b2 upload-file "zfs-rent" "dist/zz.bin" "zz"
+# download b2 binary if necessary
+b2="/tmp/b2.bin"
+if [ ! -f "$b2" ]; then
+    curl -o "$b2" "https://f000.backblazeb2.com/file/backblazefiles/b2/cli/linux/b2"
+    chmod +x "$b2"
+fi
+
+# login
+"$b2" authorize-account "$b2_key_id" "$b2_app_key"
+
+# upload
+"$b2" upload-file "zfs-rent" "dist/zz.bin" "zz"
