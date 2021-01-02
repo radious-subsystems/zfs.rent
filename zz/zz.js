3bfa048301e71fce0c03463283f8aad6f1929f72
Ryan Jacobs <ryan@rmj.us>
2021-01-01 21:36:39 -0800

    correct path

--- zz/zz.js
+++ zz/zz.js
@@ -11,7 +11,7 @@ const {readFiles, uts} = require("./lib/helpers");
 const fetch = require("node-fetch");
 
 (async function() {
-  const res = await fetch("https://zfs.rent/api/v0/hello/world");
+  const res = await fetch("https://zfs.rent/api/v0/user/info");
   const jsn = await res.json();
   console.log(jsn.res.version);
 })()
