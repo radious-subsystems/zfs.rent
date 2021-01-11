313e2522a2d7c764d315629c45c75cb5ce83082d
Ryan Jacobs <ryan@rmj.us>
2021-01-11 05:20:10 -0800

    only display month

--- backend/netlify_functions/user/bandwidth-monthly.js
+++ backend/netlify_functions/user/bandwidth-monthly.js
@@ -24,7 +24,7 @@ exports.handler = async function (event, context) {
   // fetch monthly bandwidth report
   const rows = (await pool.query(`
     SELECT
-      date_trunc('month', date)::date::text as month,
+      to_char(created_at, 'YYYY-MM') as month,
       domain,
       sum(gb_consumed) gb_consumed
     FROM _user_bandwidth
