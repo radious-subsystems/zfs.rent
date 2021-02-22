5b19b2c0b102ac3f2655176735d79f5cd9835148
Ryan Jacobs <ryan@rmj.us>
2021-02-22 03:29:14 -0800

    monthly_bw.sql

---
+++ monthly_bw.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE VIEW _user_bandwidth_by_month AS (
+  SELECT
+    to_char(date, 'YYYY-MM') period,
+    domain,
+    SUM(gb_consumed)
+  FROM _user_bandwidth
+  GROUP BY period, domain
+);
+
+CREATE MATERIALIZED VIEW __user_bandwidth_by_month AS (
+  SELECT * FROM _user_bandwidth_by_month
+);
+
+COMMIT; 
