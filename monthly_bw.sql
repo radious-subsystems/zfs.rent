3fa326a1c6b667483c3a2acba4b3c49235ec52b6
Ryan Jacobs <ryan@rmj.us>
2021-02-22 03:31:11 -0800

    name col

--- monthly_bw.sql
+++ monthly_bw.sql
@@ -1,10 +1,13 @@
 BEGIN;
 
+DROP MATERIALIZED VIEW __user_bandwidth_by_month;
+DROP VIEW _user_bandwidth_by_month;
+
 CREATE VIEW _user_bandwidth_by_month AS (
   SELECT
     to_char(date, 'YYYY-MM') period,
     domain,
-    SUM(gb_consumed)
+    SUM(gb_consumed) as gb_consumed
   FROM _user_bandwidth
   GROUP BY period, domain
 );
