86f5f767df0a44a61551a6167a1529426fa6b250
Ryan Jacobs <ryan@rmj.us>
2021-11-22 05:17:44 -0800

    proc uptime

--- backend/db/tables/hypervisor.sql
+++ backend/db/tables/hypervisor.sql
@@ -6,7 +6,10 @@ CREATE TABLE IF NOT EXISTS hypervisor (
   uptime_cmd TEXT
 );
 
-ALTER TABLE hypervisor
-  ADD COLUMN IF NOT EXISTS uptime_cmd_pretty TEXT;
+ALTER TABLE hypervisor ADD  COLUMN IF NOT EXISTS uptime_cmd_pretty TEXT;
 
+ALTER TABLE hypervisor DROP COLUMN IF EXISTS uptime_cmd_pretty;
+ALTER TABLE hypervisor ADD  COLUMN IF NOT EXISTS proc_uptime TEXT;
+
+-- ROLLBACK;
 COMMIT;
