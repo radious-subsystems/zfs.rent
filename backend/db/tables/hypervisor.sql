8e1a706f7b3aff6b5dcaa33098e848bf95837755
Ryan Jacobs <ryan@rmj.us>
2021-11-22 05:13:39 -0800

    sql: add uptime pretty cmd

--- backend/db/tables/hypervisor.sql
+++ backend/db/tables/hypervisor.sql
@@ -1,5 +1,12 @@
+BEGIN;
+
 CREATE TABLE IF NOT EXISTS hypervisor (
   hostname   TEXT PRIMARY KEY,
   last_seen  TIMESTAMPTZ,
   uptime_cmd TEXT
 );
+
+ALTER TABLE hypervisor
+  ADD COLUMN IF NOT EXISTS uptime_cmd_pretty TEXT;
+
+COMMIT;
