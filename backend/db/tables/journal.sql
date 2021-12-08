e27176ac4beee113011bdd711c3d4c309d1a87b4
Ryan Jacobs <ryan@rmj.us>
2021-12-07 19:03:43 -0800

    ts

--- backend/db/tables/journal.sql
+++ backend/db/tables/journal.sql
@@ -12,5 +12,5 @@ COMMIT;
 
 -- Tue Dec  7 06:44:43 PM PST 2021
 CREATE INDEX journal_pg_bad_connection_idx
-  ON journal (id, ts)
+  ON journal (ts)
   WHERE content::TEXT LIKE '%PG::ConnectionBad%';
