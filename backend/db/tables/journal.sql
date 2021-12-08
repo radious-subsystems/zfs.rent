74120aa7f1f4fd2d760e2d6864b29e95857c4eec
Ryan Jacobs <ryan@rmj.us>
2021-12-07 19:23:52 -0800

    fix index

--- backend/db/tables/journal.sql
+++ backend/db/tables/journal.sql
@@ -13,4 +13,4 @@ COMMIT;
 -- Tue Dec  7 06:44:43 PM PST 2021
 CREATE INDEX journal_pg_bad_connection_idx
   ON journal (ts)
-  WHERE content::TEXT LIKE '%PG::ConnectionBad%';
+  WHERE content->>'MESSAGE' LIKE '%PG::ConnectionBad%';
