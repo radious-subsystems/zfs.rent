15c56e68f3a544572317435caaa97c438527deea
Ryan Jacobs <ryan@rmj.us>
2022-01-11 05:39:05 -0800

    change up index order

--- backend/db/tables/journal.sql
+++ backend/db/tables/journal.sql
@@ -17,4 +17,4 @@ CREATE INDEX journal_pg_bad_connection_idx
 
 -- Tue Jan 11 05:01:36 AM PST 2022
 CREATE INDEX journal_hostname_idx
-  ON journal ((content->>'HOSTNAME'), ts);
+  ON journal (ts, (content->>'_HOSTNAME'));
