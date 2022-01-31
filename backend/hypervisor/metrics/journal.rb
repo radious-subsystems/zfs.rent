42223ff7c6e76cc93f40ba10a3e8c52f01914853
Ryan Jacobs <ryan@rmj.us>
2022-01-31 03:28:21 -0800

    use journal_ht

--- backend/hypervisor/metrics/journal.rb
+++ backend/hypervisor/metrics/journal.rb
@@ -12,26 +12,28 @@ def params(rows: nil, columns: nil)
   return x.join(",\n")
 end
 
-def log_journal(pg, line_count: 10)
+def log_journal(pg, line_count: 100)
   rows  = []
   lines = `journalctl --output json --reverse --lines=#{line_count}`
 
   lines.each_line do |content|
     data = JSON.parse(content)
-    id   = data["_BOOT_ID"] + "." + data["__REALTIME_TIMESTAMP"]
 
-    rows << [id, content]
+    boot_id = data["_BOOT_ID"]
+    ts      = Time.at(data["__REALTIME_TIMESTAMP"].to_i / 1e6)
+
+    rows << [ts, boot_id, content]
   end
 
   query = %{
-    INSERT INTO journal
-    (id, content)
+    INSERT INTO journal_ht
+    (ts, boot_id, content)
     VALUES
     #{params(rows: rows.length, columns: rows[0].length)}
     ON CONFLICT DO NOTHING
-    RETURNING ts, id;
+    RETURNING ts, boot_id;
   }
 
   res = pg.exec(query, rows.flatten)
-  puts "table journal: inserted #{res.to_a.count} rows."
+  puts "table journal_ht: inserted #{res.to_a.count} rows."
 end
