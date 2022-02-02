b7fb00ffaa6b2c0b09ba6dc6bc3c92ae67fd54c9
Ryan Jacobs <ryan@rmj.us>
2022-02-01 23:25:26 -0800

    ruby format

--- backend/hypervisor/metrics/metrics.rb
+++ backend/hypervisor/metrics/metrics.rb
@@ -34,34 +34,34 @@ end
 # post metrics
 rows = pg.exec(%{SELECT * FROM metrics_cmd}).to_a
 rows.each do |row|
-    # fork and execute command --> insert to postgres
-    pid = fork do
-        cmd = row["command"]
+  # fork and execute command --> insert to postgres
+  pid = fork do
+    cmd = row["command"]
 
-        stdout, stderr, status = Open3.capture3(cmd)
-        puts status
+    stdout, stderr, status = Open3.capture3(cmd)
+    puts status
 
-        stdout = clean(stdout)
-        stderr = clean(stderr)
+    stdout = clean(stdout)
+    stderr = clean(stderr)
 
-        # map stdout to the correct column
-        txt = (row["output_type"] == "text") ? stdout : nil
-        jsn = (row["output_type"] == "json") ? stdout : nil
-        xml = (row["output_type"] == "xml")  ? stdout : nil
+    # map stdout to the correct column
+    txt = (row["output_type"] == "text") ? stdout : nil
+    jsn = (row["output_type"] == "json") ? stdout : nil
+    xml = (row["output_type"] == "xml")  ? stdout : nil
 
-        # insert
-        begin
-            pg = PG.connect()
-            pg.exec(%{
-                INSERT INTO metrics
-                (hostname, cmd, status, stderr, txt, jsn, xml)
-                VALUES ($1, $2, $3, $4, $5, $6, $7);
-            }, [hostname, cmd, status, stderr, txt, jsn, xml])
-        ensure
-            puts "pg.close"
-            pg.close
-        end
+    # insert
+    begin
+      pg = PG.connect()
+      pg.exec(%{
+        INSERT INTO metrics
+        (hostname, cmd, status, stderr, txt, jsn, xml)
+        VALUES ($1, $2, $3, $4, $5, $6, $7);
+      }, [hostname, cmd, status, stderr, txt, jsn, xml])
+    ensure
+      puts "pg.close"
+      pg.close
     end
+  end
 end
 
 Process.waitall
